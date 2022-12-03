3b92553f2606a936e6ef31bb441a4c71e16da6ea
Vadym <besarabvadim2@gmail.com>
2022-12-03 13:39:28 +0200

    kgfkfkhgfh

---
+++ src/ JS/teamModal.js
@@ -0,0 +1,18 @@
+
+const modal = document.getElementById("teamModal");
+const btn = document.getElementById("myBtn");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
