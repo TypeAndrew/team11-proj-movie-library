0d5ebba8884ad52918d983c80498cc654fbe8c16
TypeAndrew <andy.shular@gmail.com>
2022-12-07 13:21:53 +0200

    fix bug modal window not open on find

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,22 +1,22 @@
 import MovieApiService from './movies-service';
 import addToLocalStorage from './localStorage-logic';
 import {
-  formEl,
-  movieSection,
-  modalMovie,
-  warningField,
-  galleryEl,
-  poster,
-  movieTitle,
-  averageVotes,
-  totalVotes,
-  popularity,
-  originalTitle,
-  genres,
-  overview,
-  btnAddWatched,
-  btnAddQueue,
-  closeBtn,
+    formEl,
+    movieSection,
+    modalMovie,
+    warningField,
+    galleryEl,
+    poster,
+    movieTitle,
+    averageVotes,
+    totalVotes,
+    popularity,
+    originalTitle,
+    genres,
+    overview,
+    btnAddWatched,
+    btnAddQueue,
+    closeBtn,
 } from './refs';
 
 const movieService = new MovieApiService();
@@ -27,11 +27,11 @@ const language = 'en-US';
 const include_adult = false;
 
 function createMarkup(response) {
-  let markup = '';
-  response.data.results.map(element => {
-    let strGenres = movieService.findGenresById(element);
+    let markup = '';
+    response.data.results.map(element => {
+        let strGenres = movieService.findGenresById(element);
 
-    markup += `<li class="movie__card">
+        markup += `<li class="movie__card">
     <a href="https://www.themoviedb.org/t/p/original/${
       element.backdrop_path
     }"><img class="movie__poster" src="https://www.themoviedb.org/t/p/original/${
@@ -45,52 +45,52 @@ function createMarkup(response) {
     )}</span></p>
     </div>
     </li>`;
-  });
-  return markup;
+    });
+    return markup;
 }
 
 export function createModalMarkup(element) {
-  let strGenres = movieService.findGenresById(element);
+    let strGenres = movieService.findGenresById(element);
 
-  if (element.poster_path !== null) {
-    poster.src = `https://www.themoviedb.org/t/p/original/${element.poster_path}`;
-  }
-  poster.alt = `${element.original_title}`;
-  movieTitle.textContent = `${element.title}`;
-  averageVotes.textContent = `${element.vote_average}`;
-  totalVotes.textContent = `${element.vote_count}`;
-  popularity.textContent = `${element.popularity}`;
-  originalTitle.textContent = `${element.original_title}`;
-  genres.textContent = `${strGenres}`;
-  overview.textContent = `${element.overview}`;
+    if (element.poster_path !== null) {
+        poster.src = `https://www.themoviedb.org/t/p/original/${element.poster_path}`;
+    }
+    poster.alt = `${element.original_title}`;
+    movieTitle.textContent = `${element.title}`;
+    averageVotes.textContent = `${element.vote_average}`;
+    totalVotes.textContent = `${element.vote_count}`;
+    popularity.textContent = `${element.popularity}`;
+    originalTitle.textContent = `${element.original_title}`;
+    genres.textContent = `${strGenres}`;
+    overview.textContent = `${element.overview}`;
 
-  addToLocalStorage(element);
+    addToLocalStorage(element);
 }
 
 async function getMovies(request) {
-  try {
-    const response = await movieService.fetchMovies(request);
-    if (response.data.total_results === 0) {
-      warningField.textContent =
-        'Sorry, there are no movies matching your search query. Please try again.';
-      setTimeout(() => (warningField.textContent = ''), 3000);
-    } else {
-      galleryEl.innerHTML = createMarkup(response);
-    }
-  } catch (error) {
-    warningField.textContent = 'Please write something in the box :)';
-    setTimeout(() => (warningField.textContent = ''), 3000);
+    try {
+        const response = await movieService.fetchMovies(request);
+        if (response.data.total_results === 0) {
+            warningField.textContent =
+                'Sorry, there are no movies matching your search query. Please try again.';
+            setTimeout(() => (warningField.textContent = ''), 3000);
+        } else {
+            galleryEl.innerHTML = createMarkup(response);
+        }
+    } catch (error) {
+        warningField.textContent = 'Please write something in the box :)';
+        setTimeout(() => (warningField.textContent = ''), 3000);
 
-    console.log(error);
-  }
+        console.log(error);
+    }
 }
 
 function onFormSubmit(event) {
-  event.preventDefault();
-  movieService.query = formEl.elements.searchmovies.value;
-  let request = `${BASE_URL}search/movie?api_key=${API_KEY}&language=${language}&page=${movieService.page}&include_adult=${include_adult}&query=${movieService.query}`;
+    event.preventDefault();
+    movieService.query = formEl.elements.searchmovies.value;
 
-  getMovies(request);
+    let request = `${BASE_URL}search/movie?api_key=${API_KEY}&language=${language}&page=${movieService.page}&include_adult=${include_adult}&query=${movieService.query}`;
+    getMovies(request);
 }
 
 formEl.addEventListener('submit', onFormSubmit);
