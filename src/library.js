0e10c8a414d5e2b9591cee5e87d42c676e843c63
Daniil Plakhotnuk <dplakhotnuk@gmail.com>
2022-12-09 10:44:57 +0200

    fixes team modal in library

--- src/library.js
+++ src/library.js
@@ -1,5 +1,5 @@
+import './js/teamModal.js';
 import './js/spinner';
 import './js/colorTheme';
 import './js/addFilmLibrary';
 import './js/movieModal';
-import { teamModal } from './js/teamModal.js';
